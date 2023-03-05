487cd629b5791a4722fd78cd172086cb70cfdb78
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-03-05 21:55:56 +0900

    feat: 9.5 ~ 9.7 - nomflix

--- nomflix/src/index.tsx
+++ nomflix/src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -55,8 +56,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    color: black;
+    color: ${(props) => props.theme.white.darker};
     line-height: 1.2;
+    background-color: black;
   }
   a {
     text-decoration: none;
@@ -68,13 +70,17 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const client = new QueryClient();
+
 root.render(
   // <React.StrictMode>
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </RecoilRoot>
   // </React.StrictMode>
 );
